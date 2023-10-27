d7b5ac93bcfd276a73b3b284107d98f7a91a0f69
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2023-10-27 18:13:42 +0200

    minor

--- src/dphpc_sinv/generate_test_matrices.py
+++ src/dphpc_sinv/generate_test_matrices.py
@@ -1,8 +1,16 @@
+"""
+@author: Vincent Maillou (vmaillou@iis.ee.ethz.ch)
+@date: 2023-09
+
+Copyright 2023 under ETH Zurich DPHPC project course. All rights reserved.
+"""
+
 import numpy as np
+import matplotlib.pyplot as plt
 
 SEED = 63
 MAT_SIZE = 10
-BLOCKSIZE = 1
+BLOCKSIZE = 2
 
 
 
@@ -17,7 +25,7 @@ def write_matrix_to_file(
     path_to_file: str,
     matrix: np.ndarray,
     matrix_size: int,
-    blocksize: int
+    blocksize: int,
 ):
     with open(path_to_file, "wb") as f:
         f.write(matrix.tobytes())
@@ -31,7 +39,77 @@ def print_matrix(
         for j in range(matrix_size):
             print(matrix[i, j], end=" ")
         print()
+        
+def show_matrix(
+    matrix: np.ndarray,
+    matrix_to_compare_to: np.ndarray = None,
+):
+    plt.matshow(matrix.real)
+    if matrix_to_compare_to is not None:
+        plt.matshow(matrix_to_compare_to.real)
+    plt.show()
     
+
+
+def extract_diagonal_blocks(
+    matrix: np.ndarray,
+    matrix_size: int,
+    blocksize: int,
+) -> np.ndarray:
+    """ Extract the diagonal blocks of a dense matrix and store them in a 
+    contiguous array.
+    """
+    num_blocks = matrix_size // blocksize
+    blocks = np.empty((blocksize, num_blocks*blocksize), dtype=matrix.dtype)
+    for i in range(num_blocks):
+        row_start = i * blocksize
+        row_end = row_start + blocksize
+        col_start = i * blocksize
+        col_end = col_start + blocksize
+        blocks[0:blocksize, row_start:row_end] = matrix[row_start:row_end, col_start:col_end]
+    return blocks
+
+
+
+def extract_offdiagonal_blocks(
+    matrix: np.ndarray,
+    matrix_size: int,
+    blocksize: int,
+    n_off_diag: int,
+) -> np.ndarray:
+    """ Extract the n_off_diag off-diagonal blocks of a dense matrix and store
+    them in a contiguous array.
+    """
+    num_blocks = matrix_size // blocksize - abs(n_off_diag)
+    blocks = np.empty((blocksize, num_blocks*blocksize), dtype=matrix.dtype)
+    
+    if n_off_diag > 0:
+        for i in range(num_blocks):
+            row_start = i * blocksize
+            row_end   = row_start + blocksize
+            col_start = (i + n_off_diag) * blocksize
+            col_end   = col_start + blocksize
+            blocks[0:blocksize, row_start:row_end] = matrix[row_start:row_end, col_start:col_end]
+    else:
+        for i in range(num_blocks):
+            row_start = (i - n_off_diag) * blocksize
+            row_end   = row_start + blocksize
+            col_start = i * blocksize
+            col_end   = col_start + blocksize
+            blocks[0:blocksize, col_start:col_end] = matrix[row_start:row_end, col_start:col_end]        
+    
+    return blocks
+
+
+def write_matrix_parameters(
+    path_to_file: str,
+    matrix_size: int,
+    blocksize: int,
+):
+    with open(path_to_file, "w") as f:
+        f.write(str(matrix_size) + "\n")
+        f.write(str(blocksize) + "\n")
+
     
 if __name__ == "__main__":
     # Generate random matrix
@@ -40,16 +118,33 @@ if __name__ == "__main__":
     # Compute inverse
     inv_matrix = np.linalg.inv(matrix)
 
-    # Save inverse matrix to binary file
+
+    # Extract diagonal and off-diagonal blocks
+    matrix_diag_blk = extract_diagonal_blocks(matrix, MAT_SIZE, BLOCKSIZE)
+    matrix_upper_blk = extract_offdiagonal_blocks(matrix, MAT_SIZE, BLOCKSIZE, 1)
+    matrix_lower_blk = extract_offdiagonal_blocks(matrix, MAT_SIZE, BLOCKSIZE, -1)
+    
+    matrix_diag_blk = extract_diagonal_blocks(inv_matrix, MAT_SIZE, BLOCKSIZE)
+    matrix_upper_blk = extract_offdiagonal_blocks(inv_matrix, MAT_SIZE, BLOCKSIZE, 1)
+    matrix_lower_blk = extract_offdiagonal_blocks(inv_matrix, MAT_SIZE, BLOCKSIZE, -1)
+
+
+    # Save matrices to file
     path_to_file = "../../tests/tests_cases/"
     
-    filename = "matrix_0.bin"
+    filename = "matrix_0_diagblk.bin"
+    write_matrix_to_file(path_to_file+filename, matrix, MAT_SIZE, 1)
+    filename = "matrix_0_upperblk.bin"
+    write_matrix_to_file(path_to_file+filename, matrix, MAT_SIZE, 1)
+    filename = "matrix_0_lowerblk.bin"
     write_matrix_to_file(path_to_file+filename, matrix, MAT_SIZE, 1)
     
-    filename = "matrix_0_inverse.bin"
+    filename = "matrix_0_inverse_diagblk.bin"
+    write_matrix_to_file(path_to_file+filename, inv_matrix, MAT_SIZE, 1)
+    filename = "matrix_0_inverse_upperblk.bin"
+    write_matrix_to_file(path_to_file+filename, inv_matrix, MAT_SIZE, 1)
+    filename = "matrix_0_inverse_lowerblk.bin"
     write_matrix_to_file(path_to_file+filename, inv_matrix, MAT_SIZE, 1)
 
-    #print_matrix(matrix, MAT_SIZE)
-    
-    
-    
+    filename = "mat_parameters_0.txt"
+    write_matrix_parameters(path_to_file+filename, MAT_SIZE, BLOCKSIZE)
--- src/dphpc_sinv/rgf_denseblocks.c
+++ src/dphpc_sinv/rgf_denseblocks.c
@@ -1,7 +1,15 @@
+/*
+@author: Vincent Maillou (vmaillou@iis.ee.ethz.ch)
+@date: 2023-09
+
+Copyright 2023 under ETH Zurich DPHPC project course. All rights reserved.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <complex.h>
 
+//#include "utils.h"
 
 
 int load_matrix(
@@ -47,21 +55,88 @@ void print_matrix(
 }
 
 
-int main() {
-    double complex *matrix;
-    char filename[] = "../../tests/tests_cases/matrix_0.bin";
+int load_matrix_parameters(
+    char *filename, 
+    unsigned int *matrice_size, 
+    unsigned int *blocksize)
+{
+    FILE *fp;
 
-    unsigned int mat_rows = 10;
-    unsigned int mat_cols = 10;
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+        printf("Error opening file\n");
+        return 1;
+    }
 
-    load_matrix(filename, &matrix, mat_rows, mat_cols);
+    fscanf(fp, "%u %u", matrice_size, blocksize);
 
-    print_matrix(matrix, mat_rows, mat_cols);
-
-    free_matrix(matrix);
+    fclose(fp);
 
     return 0;
 }
+
+
+int main() {
+    // Get matrix parameters
+    char f_matparam[] = "../../tests/tests_cases/mat_parameters_0.txt";
+    unsigned int matrice_size;
+    unsigned int blocksize;
+
+    load_matrix_parameters(f_matparam, &matrice_size, &blocksize);
+    
+
+    // Load matrix to invert
+    double complex *matrix_diagblk;
+    char f_mat_diagblk[] = "../../tests/tests_cases/matrix_0_diagblk.bin";
+    load_matrix(f_mat_diagblk, &matrix_diagblk, blocksize, matrice_size);
+
+    double complex *matrix_upperblk;
+    char f_mat_upperblk[] = "../../tests/tests_cases/matrix_0_upperblk.bin";
+    load_matrix(f_mat_upperblk, &matrix_upperblk, blocksize, matrice_size-blocksize);
+
+    double complex *matrix_lowerblk;
+    char f_mat_lowerblk[] = "../../tests/tests_cases/matrix_0_lowerblk.bin";
+    load_matrix(f_mat_lowerblk, &matrix_lowerblk, blocksize, matrice_size-blocksize);
+
+
+    // Load reference solution of the matrix inverse
+    double complex *matrix_inv_diagblk;
+    char f_mat_inv_diagblk[] = "../../tests/tests_cases/matrix_0_inverse_diagblk.bin";
+    load_matrix(f_mat_inv_diagblk, &matrix_inv_diagblk, blocksize, matrice_size);
+
+    double complex *matrix_inv_upperblk;
+    char f_mat_inv_upperblk[] = "../../tests/tests_cases/matrix_0_inverse_upperblk.bin";
+    load_matrix(f_mat_inv_upperblk, &matrix_inv_upperblk, blocksize, matrice_size-blocksize);
+    
+    double complex *matrix_inv_lowerblk;
+    char f_mat_inv_lowerblk[] = "../../tests/tests_cases/matrix_0_inverse_lowerblk.bin";
+    load_matrix(f_mat_inv_lowerblk, &matrix_inv_lowerblk, blocksize, matrice_size-blocksize);
+
+    // ----- END OF INIT SECTION -----
+
+
+
+
+
+
+
+
+
+
+
+    // ----- START OF CLEANING SECTION -----
+
+    free_matrix(matrix_diagblk);
+    free_matrix(matrix_upperblk);
+    free_matrix(matrix_lowerblk);
+    free_matrix(matrix_inv_diagblk);
+    free_matrix(matrix_inv_upperblk);
+    free_matrix(matrix_inv_lowerblk);
+
+    return 0;
+}
+
+
 
 
 

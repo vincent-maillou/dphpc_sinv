dcefe399fd85b20bfe3d673699cd0d7590368982
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2023-10-27 10:56:20 +0200

    Just basics functions works

--- src/dphpc_sinv/rgf_denseblocks.c
+++ src/dphpc_sinv/rgf_denseblocks.c
@@ -2,14 +2,11 @@
 #include <stdlib.h>
 #include <complex.h>
 
-#define ROWS 10
-#define COLS 10
-
 
 
 int load_matrix(
     char *filename, 
-    double complex *matrix, 
+    double complex **matrix, 
     int rows, 
     int cols)
 {
@@ -21,11 +18,18 @@ int load_matrix(
         return 1;
     }
 
-    matrix = (double complex *)malloc(rows * cols * sizeof(double complex));
+    *matrix = malloc(rows * cols * sizeof(double complex));
 
-    fread(matrix, sizeof(double complex), ROWS * COLS, fp);
+    fread(*matrix, sizeof(double complex), rows * cols, fp);
 
     fclose(fp);
+}
+
+
+void free_matrix(
+    double complex *matrix)
+{
+    free(matrix);
 }
 
 
@@ -34,51 +38,33 @@ void print_matrix(
     int rows, 
     int cols)
 {
+    // Access matrix elements
     for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++){
+        for (int j = 0; j < cols; j++)
             printf("%f + %fi ", creal(matrix[i * cols + j]), cimag(matrix[i * cols + j]));
-        }
         printf("\n");
     }
 }
-
 
 
 int main() {
     double complex *matrix;
     char filename[] = "../../tests/tests_cases/matrix_0.bin";
 
-    load_matrix(filename, matrix, ROWS, COLS);
+    unsigned int mat_rows = 10;
+    unsigned int mat_cols = 10;
 
-    print_matrix((double complex *)matrix, ROWS, COLS);
+    load_matrix(filename, &matrix, mat_rows, mat_cols);
+
+    print_matrix(matrix, mat_rows, mat_cols);
+
+    free_matrix(matrix);
 
     return 0;
 }
 
 
 
-/* int main() {
-    FILE *fp;
-    char filename[] = "../../tests/tests_cases/matrix_0.bin";
-    double complex matrix[ROWS][COLS];
 
-    fp = fopen(filename, "rb");
-    if (fp == NULL) {
-        printf("Error opening file\n");
-        return 1;
-    }
 
-    fread(matrix, sizeof(double complex), ROWS * COLS, fp);
 
-    fclose(fp);
-
-    // Access matrix elements
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++) {
-            printf("%f + %fi ", creal(matrix[i][j]), cimag(matrix[i][j]));
-        }
-        printf("\n");
-    }
-
-    return 0;
-} */
